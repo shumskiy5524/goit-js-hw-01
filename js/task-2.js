191a7e4fdb90d8c77b46e99e8e52f6b59b0292d3
shumskiy <shumskiparents@gmail.com>
2025-10-18 12:16:01 +0200

    js

--- js/task-2.js
+++ js/task-2.js
@@ -6,6 +6,6 @@ const shippingFee = deliveryFee;
  const message = "Shipping to " + deliveryCountry + " will cost " + totalPrice + " credits";
  return message;
 }
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20)); 
+console.log(getShippingMessage("Sweden", 100, 20)); 
