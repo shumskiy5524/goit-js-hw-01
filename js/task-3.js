669e02ef11598cfb6fc5b9436f28126d2d04b89b
shumskiy <shumskiparents@gmail.com>
2025-10-18 12:18:08 +0200

    js

--- js/task-3.js
+++ js/task-3.js
@@ -5,6 +5,6 @@ function getElementWidth(content,padding,border){
      const totalWidth = contentValue + (paddingValue * 2) + (borderValue * 2);
  return totalWidth;
 }
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px")); 
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
